19eb55d4f276c2d51f7d5342725f526080608611
Roman Shtylman <shtylman@gmail.com>
2014-03-05 16:00:17 -0500

    change to support express 4 and use Router() for subroutes

--- templates/js/app.js
+++ templates/js/app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes');
-var users = require('./routes/user');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -21,10 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());{css}
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
 
-app.get('/', routes.index);
-app.get('/users', users.list);
+app.use('/', routes);
+app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
