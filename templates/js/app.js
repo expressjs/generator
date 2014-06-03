a90a755d85fed9b71506bdba1563bc8ef93ea101
Jon Petto <jonpetto@gmail.com>
2014-06-03 16:41:32 -0500

    Replace static-favicon with serve-favicon.

--- templates/js/app.js
+++ templates/js/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -14,7 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '{views}');
 
-app.use(favicon());
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
