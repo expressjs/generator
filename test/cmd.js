5c809ee3eb59df0ac4c059a991f5a2b352d398e9
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-08-24 16:46:39 -0400

    tests: add basic integration test
    closes #37

---
+++ test/cmd.js
@@ -0,0 +1,162 @@
+
+var assert = require('assert');
+var exec = require('child_process').exec;
+var mkdirp = require('mkdirp');
+var path = require('path');
+var request = require('supertest');
+var rimraf = require('rimraf');
+var spawn = require('child_process').spawn;
+
+var binPath = path.resolve(__dirname, '../bin/express');
+var tempDir = path.resolve(__dirname, '../temp');
+
+describe('express(1)', function () {
+  before(function (done) {
+    this.timeout(30000);
+    cleanup(done);
+  });
+
+  after(function (done) {
+    this.timeout(30000);
+    cleanup(done);
+  });
+
+  describe('(no args)', function () {
+    var dir;
+
+    before(function (done) {
+      createEnvironment(function (err, newDir) {
+        if (err) return done(err);
+        dir = newDir;
+        done();
+      });
+    });
+
+    after(function (done) {
+      this.timeout(30000);
+      cleanup(dir, done);
+    });
+
+    it('should create basic app', function (done) {
+      run(dir, [], function (err, stdout) {
+        if (err) return done(err);
+        var files = parseCreatedFiles(stdout, dir);
+        assert.equal(files.length, 17);
+        assert.notEqual(files.indexOf('bin/www'), -1);
+        assert.notEqual(files.indexOf('app.js'), -1);
+        assert.notEqual(files.indexOf('package.json'), -1);
+        done();
+      });
+    });
+
+    it('should have installable dependencies', function (done) {
+      this.timeout(20000);
+      npmInstall(dir, done);
+    });
+
+    it('should export an express app from app.js', function () {
+      var file = path.resolve(dir, 'app.js');
+      var app = require(file);
+      assert.equal(typeof app, 'function');
+      assert.equal(typeof app.handle, 'function');
+    });
+
+    it('should respond to HTTP request', function (done) {
+      var file = path.resolve(dir, 'app.js');
+      var app = require(file);
+
+      request(app)
+      .get('/')
+      .expect(200, done);
+    });
+  });
+});
+
+function cleanup(dir, callback) {
+  if (typeof dir === 'function') {
+    callback = dir;
+    dir = tempDir;
+  }
+
+  rimraf(tempDir, function (err) {
+    callback(err);
+  });
+}
+
+function createEnvironment(callback) {
+  var num = process.pid + Math.random();
+  var dir = path.join(tempDir, ('app-' + num));
+
+  mkdirp(dir, function ondir(err) {
+    if (err) return callback(err);
+    callback(null, dir);
+  });
+}
+
+function npmInstall(dir, callback) {
+  exec('npm install', {cwd: dir}, function (err, stderr) {
+    if (err) {
+      err.message += stderr;
+      callback(err);
+      return;
+    }
+
+    callback();
+  });
+}
+
+function parseCreatedFiles(output, dir) {
+  var files = [];
+  var lines = output.split(/[\r\n]+/);
+  var match;
+
+  for (var i = 0; i < lines.length; i++) {
+    if ((match = /create.*?: (.*)$/.exec(lines[i]))) {
+      var file = match[1];
+
+      if (dir) {
+        file = path.resolve(dir, file);
+        file = path.relative(dir, file);
+      }
+
+      file = file.replace(/\\/g, '/');
+      files.push(file);
+    }
+  }
+
+  return files;
+}
+
+function run(dir, args, callback) {
+  var argv = [binPath].concat(args);
+  var chunks = [];
+  var exec = process.argv[0];
+  var stderr = [];
+
+  var child = spawn(exec, argv, {
+    cwd: dir
+  });
+
+  child.stdout.on('data', function ondata(chunk) {
+    chunks.push(chunk);
+  });
+  child.stderr.on('data', function ondata(chunk) {
+    stderr.push(chunk);
+  });
+
+  child.on('error', callback);
+  child.on('exit', function onexit() {
+    var err = null;
+    var stdout = Buffer.concat(chunks)
+      .toString('utf8')
+      .replace(/\x1b\[(\d+)m/g, '_color_$1_');
+
+    try {
+      assert.equal(Buffer.concat(stderr).toString('utf8'), '');
+    } catch (e) {
+      err = e;
+    }
+
+    callback(err, stdout);
+  });
+}
