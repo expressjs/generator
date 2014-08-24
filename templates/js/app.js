e14489181f5e73b085fefb159eeae96f29bbc6b4
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-08-24 10:50:43 -0400

    gen: update middleware versions

--- templates/js/app.js
+++ templates/js/app.js
@@ -18,21 +18,21 @@ app.set('view engine', '{views}');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());{css}
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
-/// catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-/// error handlers
+// error handlers
 
 // development error handler
 // will print stacktrace
