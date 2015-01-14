9cf6540dc0ed56b43bc8c9288701ea91e934abc3
chirag04 <jain_chirag04@yahoo.com>
2015-01-14 14:25:18 -0500

    gen: include next argument in route handlers
    closes #56
    closes #57

--- templates/js/routes/index.js
+++ templates/js/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
