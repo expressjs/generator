c3857ea60f6b01e7e5cdae043dde3ee730dbbcda
Sean Lavine <lavis88@gmail.com>
2015-06-21 14:35:50 -0700

    gen: use path.join() for serving favicon
    closes #84

--- templates/js/app.js
+++ templates/js/app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '{views}');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
