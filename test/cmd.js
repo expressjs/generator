7c939fe13a542e299d3c633fd50137236c415ac3
Douglas Christopher Wilson <doug@somethingdoug.com>
2016-06-18 00:12:52 -0400

    gen: update body-parser to ~1.15.1

--- test/cmd.js
+++ test/cmd.js
@@ -78,7 +78,7 @@ describe('express(1)', function () {
         + '    "start": "node ./bin/www"\n'
         + '  },\n'
         + '  "dependencies": {\n'
-        + '    "body-parser": "~1.15.0",\n'
+        + '    "body-parser": "~1.15.1",\n'
         + '    "cookie-parser": "~1.4.1",\n'
         + '    "debug": "~2.2.0",\n'
         + '    "express": "~4.13.4",\n'
