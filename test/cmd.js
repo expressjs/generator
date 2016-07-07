15d47e64c5126d2c0c06a7d45eff0e97ca07f88d
Santiago Aguilar <Santiago Aguilar>
2016-07-06 21:05:10 -0500

    Add vash as a view engine
    closes #124

--- test/cmd.js
+++ test/cmd.js
@@ -725,6 +725,68 @@ describe('express(1)', function () {
         .expect(404, /<h1>Not Found<\/h1>/, done)
       })
     })
+
+    describe('vash', function () {
+      var ctx = setupTestEnvironment(this.fullTitle())
+
+      it('should create basic app with vash templates', function (done) {
+        run(ctx.dir, ['--view', 'vash'], function (err, stdout) {
+          if (err) return done(err)
+          ctx.files = parseCreatedFiles(stdout, ctx.dir)
+          assert.equal(ctx.files.length, 17)
+          done()
+        })
+      })
+
+      it('should have basic files', function () {
+        assert.notEqual(ctx.files.indexOf('bin/www'), -1)
+        assert.notEqual(ctx.files.indexOf('app.js'), -1)
+        assert.notEqual(ctx.files.indexOf('package.json'), -1)
+      })
+
+      it('should have vash in package dependencies', function () {
+        var file = path.resolve(ctx.dir, 'package.json')
+        var contents = fs.readFileSync(file, 'utf8')
+        var dependencies = JSON.parse(contents).dependencies
+        assert.ok(typeof dependencies.vash === 'string')
+      })
+
+      it('should have vash templates', function () {
+        assert.notEqual(ctx.files.indexOf('views/error.vash'), -1)
+        assert.notEqual(ctx.files.indexOf('views/index.vash'), -1)
+        assert.notEqual(ctx.files.indexOf('views/layout.vash'), -1)
+      })
+
+      it('should have installable dependencies', function (done) {
+        this.timeout(30000)
+        npmInstall(ctx.dir, done)
+      })
+
+      it('should export an express app from app.js', function () {
+        var file = path.resolve(ctx.dir, 'app.js')
+        var app = require(file)
+        assert.equal(typeof app, 'function')
+        assert.equal(typeof app.handle, 'function')
+      })
+
+      it('should respond to HTTP request', function (done) {
+        var file = path.resolve(ctx.dir, 'app.js')
+        var app = require(file)
+
+        request(app)
+        .get('/')
+        .expect(200, /<title>Express<\/title>/, done)
+      })
+
+      it('should generate a 404', function (done) {
+        var file = path.resolve(ctx.dir, 'app.js')
+        var app = require(file)
+
+        request(app)
+        .get('/does_not_exist')
+        .expect(404, /<h1>Not Found<\/h1>/, done)
+      })
+    })
   })
 });
 
