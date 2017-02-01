9efbe33f9daa5aa27b3470f95f7bdec7135c958e
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-01-31 20:25:35 -0500

    gen: update serve-favicon to ~2.3.2

--- test/cmd.js
+++ test/cmd.js
@@ -74,7 +74,7 @@ describe('express(1)', function () {
         + '    "express": "~4.14.1",\n'
         + '    "jade": "~1.11.0",\n'
         + '    "morgan": "~1.7.0",\n'
-        + '    "serve-favicon": "~2.3.0"\n'
+        + '    "serve-favicon": "~2.3.2"\n'
         + '  }\n'
         + '}\n');
     });
