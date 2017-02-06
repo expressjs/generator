7026417a0cabdb80371404877b47a534ecb23f50
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-02-06 00:36:09 -0500

    gen: start to use a more generic template for file replacements

--- templates/js/app.js
+++ templates/js/app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', '{views}');
+app.set('view engine', '{view}');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
