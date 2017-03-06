8184bb2c5033feb8c24c3120cae3ad2715713c89
Toby Murray <murray.toby@gmail.com>
2017-03-05 22:58:44 -0500

    #152: Add option for no view engine
    Add '--no-view' flag to avoid installing any view engine. Serve static HTML files in place of the index and error views.

--- templates/js/app.js
+++ templates/js/app.js
@@ -4,16 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
+{requireIndexRoute}
 var users = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', '{view}');
-
+{viewEngineSetUp}
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -21,8 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());{css}
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
+{indexRoute}
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -40,7 +34,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  {renderError}
 });
 
 module.exports = app;
