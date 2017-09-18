b25cf5cb566222f96556a00c73ee618d0f882fb8
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-09-18 00:02:11 -0400

    gen: update debug to ~2.6.8

--- bin/express-cli.js
+++ bin/express-cli.js
@@ -275,7 +275,7 @@ function createApplication (name, path) {
       dependencies: {
         'body-parser': '~1.18.1',
         'cookie-parser': '~1.4.3',
-        'debug': '~2.6.3',
+        'debug': '~2.6.8',
         'express': '~4.15.2',
         'morgan': '~1.8.1',
         'serve-favicon': '~2.4.2'
