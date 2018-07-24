7e411dca87b355906ed82fab548efc0d2d178404
Bryan Crossland <bryan.crossland@target.com>
2018-07-24 10:49:31 -0500

    Fixed test.

--- test/cmd.js
+++ test/cmd.js
@@ -55,7 +55,7 @@ describe('express(1)', function () {
     it('should have events added before server start in bin/www', function () {
       var file = path.resolve(ctx.dir, 'bin/www')
       var www = fs.readFileSync(file, 'utf8')
-      var idx = www.indexOf('server.on(\'error\', onError);\nserver.on(\'listening\', onListening);\nserver.listen(port, \'0.0.0.0\');\n')
+      var idx = www.indexOf('server.on(\'error\', onError);\nserver.on(\'listening\', onListening);\nserver.listen(port);\n')
       assert.ok(idx >= 0)
     })
 
