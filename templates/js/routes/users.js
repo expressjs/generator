77e8ec43cf2626c7d446404b0fd8aa1c5abefa96
Rajat Soni <rajatsoni@drctechs.com>
2018-09-28 00:38:01 +0530

    corrected route user

--- templates/js/routes/users.js
+++ templates/js/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/users', function(req, res, next) {
   res.send('respond with a resource');
 });
 
