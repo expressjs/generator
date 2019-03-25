8b5618a8a0e467a725e6da7384e03a082bdd0119
Douglas Christopher Wilson <doug@somethingdoug.com>
2019-03-24 23:13:53 -0400

    tests: increase timeout for npm install

--- test/cmd.js
+++ test/cmd.js
@@ -14,7 +14,7 @@ var validateNpmName = require('validate-npm-package-name')
 var APP_START_STOP_TIMEOUT = 10000
 var PKG_PATH = path.resolve(__dirname, '..', 'package.json')
 var BIN_PATH = path.resolve(path.dirname(PKG_PATH), require(PKG_PATH).bin.express)
-var NPM_INSTALL_TIMEOUT = 60000
+var NPM_INSTALL_TIMEOUT = 300000 // 5 minutes
 var TEMP_DIR = utils.tmpDir()
 
 describe('express(1)', function () {
