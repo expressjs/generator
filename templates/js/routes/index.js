dd52ec1f1f972554913aea90091e8246d63adf03
Daniel Oliveira <oliveira.dcampos@gmail.com>
2019-04-25 09:25:17 -0400

    Modify of function callback for arrow function and var for const in variable express

--- templates/js/routes/index.js
+++ templates/js/routes/index.js
@@ -1,8 +1,8 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
--- templates/js/routes/users.js
+++ templates/js/routes/users.js
@@ -1,8 +1,8 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
