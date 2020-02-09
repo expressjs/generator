3bb2d8702e60b4e578adc45de82dff44a7d44c76
Yiğit Kurtçu <yiqit-35@hotmail.com>
2020-02-09 20:34:32 +0300

    Solution structure fixed.

--- bin/express-cli.js
+++ bin/express-cli.js
@@ -345,31 +345,23 @@ function createApplication (name, dir) {
 
   console.log()
   console.log('   install dependencies:')
+
+  let packageManager
   //  Checking yarn environment variable
-  if ((process.env.PATH).search('Yarn') > -1) { //  Yarn exist
-    console.log('     %s yarn install', prompt)
-    console.log()
-    console.log('   run the app:')
+  if ((process.env.PATH).search('Yarn') > -1) {
+    packageManager = 'yarn'
+  } else {
+    packageManager = 'npm'
+  }
 
-    if (launchedFromCmd()) {
-      console.log('     %s SET DEBUG=%s:* & yarn start', prompt, name)
-    } else {
-      console.log('     %s DEBUG=%s:* yarn start', prompt, name)
-    }
+  console.log(`     %s ${packageManager} install`, prompt)
+  console.log()
+  console.log('   run the app:')
 
-    console.log()
-  } else { // Yarn does not exist
-    console.log('     %s npm install', prompt)
-    console.log()
-    console.log('   run the app:')
-
-    if (launchedFromCmd()) {
-      console.log('     %s SET DEBUG=%s:* & npm start', prompt, name)
-    } else {
-      console.log('     %s DEBUG=%s:* npm start', prompt, name)
-    }
-
-    console.log()
+  if (launchedFromCmd()) {
+    console.log(`     %s SET DEBUG=%s:* & ${packageManager} start`, prompt, name)
+  } else {
+    console.log(`     %s DEBUG=%s:* ${packageManager} start`, prompt, name)
   }
 
   console.log()
