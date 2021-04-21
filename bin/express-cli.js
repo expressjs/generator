bb5f6ed5c0e3f6f947194dc9192b30c2909f9f5b
Douglas Christopher Wilson <doug@somethingdoug.com>
2021-04-21 19:04:36 -0400

    lint: apply standard 14 style

--- bin/express-cli.js
+++ bin/express-cli.js
@@ -264,7 +264,7 @@ function createApplication (name, dir) {
     case 'stylus':
       app.locals.modules.stylus = 'stylus'
       app.locals.uses.push("stylus.middleware(path.join(__dirname, 'public'))")
-      pkg.dependencies['stylus'] = '0.54.5'
+      pkg.dependencies.stylus = '0.54.5'
       break
   }
 
