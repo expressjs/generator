56e968f79ec1972de11dd939a9f9ea05125f7cc6
Douglas Christopher Wilson <doug@somethingdoug.com>
2022-03-01 19:30:46 -0500

    stylus

--- bin/express-cli.js
+++ bin/express-cli.js
@@ -281,7 +281,7 @@ function createApplication (name, dir) {
     case 'stylus':
       app.locals.modules.stylus = 'stylus'
       app.locals.uses.push("stylus.middleware(path.join(__dirname, 'public'))")
-      pkg.dependencies.stylus = '0.54.6'
+      pkg.dependencies.stylus = '0.54.7'
       break
   }
 
