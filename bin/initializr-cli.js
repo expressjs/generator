cb23e906391c54342e300ab858ed4520a18a94d7
Chijioke Ugwuanyi <chijiokeugwuanyi@Chijiokes-MacBook-Pro.local>
2024-02-08 22:06:14 +0100

    Added commander library.

--- bin/initializr-cli.js
+++ bin/initializr-cli.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-console.log("Hello world!");
+
+let { program } = require('commander');
+let path = require('path');
+let TEMPLATE_DIR = path.join(__dirname, '..', 'templates');
+let VERSION = require('../package').version;
+let services = require('./services');
+
+let APP_NAME = "express-i"; //If you change this in the future, remember to go change it in the package.json file as well.
+
+program
+	.command('basic')
+	.description('Generates a basic express application.')
+	.action(() => {
+		services.handleBasicApp();
+	});
+  
+program.parse();
---
+++ bin/services.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    handleBasicApp: () => {
+        console.log("This guy will generate basic express app.");
+    }
+}
