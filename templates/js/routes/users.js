fd6b9a9c9e2a9099a25d126c97eaa5c44c6b7498
Chijioke Ugwuanyi <chijiokeugwuanyi@Chijiokes-MacBook-Pro.local>
2024-02-08 20:23:29 +0100

    Classical first commit.

--- templates/js/routes/users.js
+++ templates/js/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	res.send('respond with a resource');
 });
 
 module.exports = router;
