0305d3dc98de7b093ad184775b0e127c8e61c0f1
Chijioke Ugwuanyi <chijiokeugwuanyi@Chijiokes-MacBook-Pro.local>
2024-04-03 23:26:27 +0100

    app name is now optional.

--- bin/initializr-cli.js
+++ bin/initializr-cli.js
@@ -14,7 +14,7 @@ let default_app_name = "hello_world"; //If you change this in the future, rememb
 program.version(VERSION);
 
 program
-	.argument('<app_name>')
+	.argument('[app_name]', 'The name you want to call the app.', default_app_name)
 	.option('-d, --dependencies <dependencies...>', 'Generates a basic express application with the given app name and the space delimted list of dependencies.')
 	.description('Generates a basic express application with the given app name.')
 	.action((app_name, options) => {
