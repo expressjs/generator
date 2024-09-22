b50e0e237f09ae92d8cd5fb5e687f0828a486d5a
saravanakumar.arumugam <saravanakumar.arumugam@aroopatech.com>
2024-09-22 22:19:43 +0530

    Es6 support auto detect the node js version. Es6 supports node v20 and above, below node v20 support old implementation

---
+++ templates/js/es6routes/users.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  try {
+    return res.send('respond with a resource')
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
