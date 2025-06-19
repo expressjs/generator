ede1a3aef4a5c16c67961266996e76ae6cc67a18
Harshal Khairnar <khairnar2960@gmail.com>
2025-06-20 00:03:29 +0530

    gen: update stylus to 0.64.0

--- bin/express-cli.js
+++ bin/express-cli.js
@@ -234,7 +234,7 @@ function createApplication (name, dir, options, done) {
     case 'stylus':
       app.locals.modules.stylus = 'stylus'
       app.locals.uses.push("stylus.middleware(path.join(__dirname, 'public'))")
-      pkg.dependencies.stylus = '0.54.5'
+      pkg.dependencies.stylus = '0.64.0'
       break
   }
 
